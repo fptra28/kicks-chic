9c4c22dbf96f15eabe1032df1b51ea8bf58f4cdc
Faturrahman Putra <92559381+fptra28@users.noreply.github.com>
2023-12-26 21:42:54 +0700

    Add files via upload

---
+++ admin/admin-dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/styles/admin.css">
+    <title>Home | Admin</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <img src="../assets/logo-knc.png" alt="Logo">
+            <span class="brand-name">Kicks & Chic</span>
+        </div>
+        <div class="side-menu">
+            <a href="/admin/admin-dashboard.php" class="active">Dashboard</a>
+            <a href="/product">Products</a>
+            <a href="/orders">Orders</a>
+            <a href="/reviews">Reviews</a>
+            <a href="/transactions">Transaction</a>
+            <a href="/hot-offers">Hot Offers</a>
+        </div>
+        <div class="account">
+            <div class="profile">
+                <i class='bx bxs-user' ></i>
+            </div>
+            <a href="/logut">
+                <button class="logout">LOG OUT</button>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
